9625eecf6ea64e9977920f774087e4fc2949260a
Lanna-27 <lalacast2000@gmail.com>
2023-03-10 13:53:10 -0500

    eliminacion modelo de grupo y pub

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,23 +5,7 @@ export interface IUser {
   name: string;
   password: string;
   verified: boolean;
-  //groups:Array<Grupo>;
-  //publications:Array<Pub>; 
-  createdAt: string;
-  updatedAt: string;
-}
-
-export interface IGroup {
-  id:number;
-  groupname: string; 
-  createdAt: string;
-  updatedAt: string;
-}
-
-export interface IPub {
-  id:number;
-  pubname: string;
-  content: string; 
+ 
   createdAt: string;
   updatedAt: string;
 }
