1497c3bc6919e39a35dc193d1ebd0c37cc3b7d15
julianVelandia <jvelandiag@unal.edu.co>
2023-03-26 22:29:36 -0500

    Principio de responsabilidad unica en jwt e inversion de dependencia para models

--- src/adapters/controllers/auth.controller.ts
+++ src/adapters/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { generateJWT } from "../../../helpers/jwt";
+import { generateJWT } from "../../helpers/jwt";
 
 export const revalidateToken = async (req: Request, res: Response) => {
   try {
