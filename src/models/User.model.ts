7a5d479972bc0f81c1fc4949a01baa0f6b6a532b
julianVelandia <jvelandiag@unal.edu.co>
2023-03-27 19:03:29 -0500

    Delete interface innecesaria

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,6 +1,5 @@
 import { Schema, model } from "mongoose";
 import { IUser } from "../domain/entities/users";
-import { IUserModel } from "../domain/interfaces/UserModel";
 
 const userSchema = new Schema(
   {
@@ -14,32 +13,4 @@ const userSchema = new Schema(
   }
 );
 
-const userModel: IUserModel = {
-  async create(user: IUser): Promise<IUser> {
-    const createdUser = await User.create(user);
-    return createdUser.toObject() as IUser;
-  },
-  async findByUsername(username: string): Promise<IUser | null> {
-    const user = await User.findOne({ username }).lean().exec();
-    return user as IUser | null;
-  },
-  async findByEmail(email: string): Promise<IUser | null> {
-    const user = await User.findOne({ email }).lean().exec();
-    return user as IUser | null;
-  },
-  async findById(id: string): Promise<IUser | null> {
-    const user = await User.findById(id).lean().exec();
-    return user as IUser | null;
-  },
-  async updateById(id: string, user: Partial<IUser>): Promise<IUser | null> {
-    const updatedUser = await User.findByIdAndUpdate(id, user, { new: true }).lean().exec();
-    return updatedUser as IUser | null;
-  },
-  async deleteById(id: string): Promise<IUser | null> {
-    const deletedUser = await User.findByIdAndDelete(id).lean().exec();
-    return deletedUser as IUser | null;
-  },
-};
-
-const User = model<IUser>("User", userSchema);
 export default model<IUser>("UserModel", userSchema);
