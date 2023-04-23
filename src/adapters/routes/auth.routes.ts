005d2a5c6a90cb28a14fa88a0e7a12c44d5d57a4
GabrielOtalora <gotalorag@unal.edu.co>
2023-04-23 12:25:17 -0500

    Revert "Creación de componentes CreateGroup-CreateEvent"
    This reverts commit ea132272efd771ac04de619e19249ac051aaccf0.

--- src/adapters/routes/auth.routes.ts
+++ src/adapters/routes/auth.routes.ts
@@ -16,9 +16,9 @@ router.post(
     // check("name", "El nombre es obligatorio").not().isEmpty(),
     check("username", "El username es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),
-    /*check("password", "La contraseña no cumple con el estándar").custom(
+    check("password", "La contraseña no cumple con el estándar").custom(
       isValidPassword
-    ),*/
+    ),
     check(
       "password_confirmation",
       "La confirmación de contraseña es obligatoria"
