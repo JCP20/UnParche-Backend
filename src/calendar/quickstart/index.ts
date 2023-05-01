28f33ccf56d4196b908320aea513ffd82a9e5c9a
jyarac <jyarac@unal.edu.co>
2023-05-01 00:14:12 -0500

    Instalacion de paquetes GCP api y modificacion index.ts Se instalaroin las librerias necesarias para conectarse a GCP se creo un index.ts que corresponde al quickstart de GCP El proyecto no funciona en este commit

--- src/calendar/quickstart/index.ts
+++ src/calendar/quickstart/index.ts
@@ -1,21 +1,3 @@
-import { config } from "dotenv";
-import express from "express";
-
-import Database from "./config/database";
-import { App } from "./app";
-
-// Load environment variables
-config();
-
-// Create db instance
-const mongoDb = new Database();
-
-// Create app instance
-const app = new App(process.env.PORT ?? "4000", mongoDb, express());
-
-// Start server
-app.start();
-
 const fs = require('fs').promises;
 const path = require('path');
 const processs = require('process');
