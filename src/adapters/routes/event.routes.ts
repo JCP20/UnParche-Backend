fcae7e78d90d25f1d98bd77dcf596e14af757ab9
Sergio Sanchez <chechoo.alejandro@gmail.com>
2023-05-25 18:57:44 -0500

    Actualizar evento lógica implementada

--- src/adapters/routes/event.routes.ts
+++ src/adapters/routes/event.routes.ts
@@ -43,7 +43,7 @@ router.get("/", getAllEvents);
 
 router.post("/", createEvent);
 
-router.put("/:id", updateEvent);
+router.patch("/update/:id", updateEvent);
 
 router.delete("/:id", deleteEvent);
 
