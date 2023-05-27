9dc19002e24626a30056a23cdc82541ebed8c660
Sergio Sanchez <chechoo.alejandro@gmail.com>
2023-05-27 00:23:21 -0500

    Se agregó el path correspondiente al .env para que pueda ser leido

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { config } from "dotenv";
 import express from "express";
+import path from "path";
 
 import Database from "./config/database";
 import { App } from "./app";
 
 // Load environment variables
-config();
+config({
+  path: path.join(__dirname, "./.env"),
+});
 
 // Create db instance
 const mongoDb = new Database();
